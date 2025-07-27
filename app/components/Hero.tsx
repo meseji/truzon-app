87e32b4930481729d9fdc69b373fc7250797a13c
your-github-username <your.email@example.com>
2025-07-27 15:40:37 +0530

    fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -33,11 +33,11 @@ export default function Hero() {
         </div>
 
         {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+        {/* <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
           <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex justify-center">
             <div className="w-1 h-3 bg-gray-400 rounded-full mt-2 animate-bounce"></div>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
